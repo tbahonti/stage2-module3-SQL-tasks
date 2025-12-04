832f0129c11ce1b93c5571ae4cf1b047255b80a3
tbahonti <tuyakbaybakhtiyar@gmail.com>
2025-12-04 09:02:32 +0500

    Add University database schema with multiple tables
    Created a database schema for University with tables for Student, Subject, Payment_type, Payment, and MARK.

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,6 @@
-
-
+CREATE DATABASE "University"
+CREATE TABLE Student ("id" BIGINT PRIMARY KEY, "name" VARCHAR NOT NULL, "birthday_date" DATE, "group" INT);
+CREATE TABLE Subject ("id" BIGINT PRIMARY KEY, "name" VARCHAR NOT NULL, "description" VARCHAR, "grade" INT);
+CREATE TABLE Payment_type ("id" BIGINT PRIMARY KEY, "name" VARCHAR NOT NULL);
+CREATE TABLE Payment ("id" BIGINT PRIMARY KEY, "type_id" BIGINT REFERENCES Payment_type(id), "amount" DECIMAL, "student_id" BIGINT REFERENCES Student(id), "payment_date" TIMESTAMP);
+CREATE TABLE MARK ("id" BIGINT PRIMARY KEY, "student_id" BIGINT REFERENCES Student(id), "subject_id" BIGINT REFERENCES Subject (id), "mark" INT);
